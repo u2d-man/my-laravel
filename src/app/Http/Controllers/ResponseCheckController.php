f948b2c4a2e1afc8ca7b40b999f96b187642dfe7
UserKazun <developer.kazu0941@gmail.com>
2023-06-04 23:25:46 +0900

    conflict

---
+++ src/app/Http/Controllers/ResponseCheckController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use stdClass;
+
+class ResponseCheckController extends Controller
+{
+    /**
+     * auto type change to json
+     *
+     * @return stdClass
+     */
+    public function returnStdClass(): stdClass
+    {
+        return (object)[
+            'message' => 'success',
+            'data' => [
+                [
+                    'name' => 'Kate',
+                    'age' => 26
+                ],
+                [
+                    'name' => 'Allen',
+                    'age' => 23
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * auto type change to json
+     *
+     * @return array
+     */
+    public function returnArray(): array
+    {
+        return [
+            'message' => 'success',
+            'data' => [
+                [
+                    'name' => 'Kate',
+                    'age' => 26,
+                    'hobby' => 'programming'
+                ],
+                [
+                    'name' => 'Allen',
+                    'age' => 23,
+                    'hobby' => 'video game'
+                ]
+            ]
+        ];
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
+use App\Http\Controllers\ResponseCheckController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+    Route::group(['prefix' => 'res_check'], function () {
+        Route::get('/stdclass', [ResponseCheckController::class, 'returnStdClass']);
+        Route::get('/array', [ResponseCheckController::class, 'returnArray']);
+    });
 });
